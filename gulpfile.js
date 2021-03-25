58bdc58cd5028e2a88dfb126c0d9c08f737dc1ff
emtadeveloper <emta137671@gmail.com>
2021-03-26 02:44:27 +0430

    install package gulp-sass and do settings for gulp / test do work

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,43 @@
 //========================================//
 
-
 // require libary
 
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const sass = require("gulp-sass");
 
 //========================================//
 
-// compile pug to html 
+// compile sass to css
+
+gulp.task("compile-sass", async function () {
+  gulp
+    .src("src/scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("src/css/"));
+});
+
+//========================================//
+
+// compile pug to html
 
 gulp.task("compile-pug", async function () {
-    return gulp.src("src/pug/index.pug").pipe(pug()).pipe(gulp.dest("src/html/"));
-  });
-  
+  return gulp.src("src/pug/index.pug").pipe(pug()).pipe(gulp.dest("src/html/"));
+});
+
 //========================================//
 
 // watch libray
 
 gulp.task("watch", async function () {
+  gulp.watch("src/scss/style.scss", gulp.series("compile-sass"));
   gulp.watch("src/pug/index.pug", gulp.series("compile-pug"));
 });
 
 //========================================//
 
-//  default libray use gulp 
+//  default libray use gulp
 
-gulp.task("default", gulp.parallel("compile-pug"));
+gulp.task("default", gulp.parallel("compile-pug","compile-sass"));
 
 //========================================//
